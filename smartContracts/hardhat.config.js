07d87551e7b121ca0f22bbe262ee5d2bbc1d3f93
Devansh Srivastava <devansh.srv@gmail.com>
2025-01-19 19:33:54 +0530

    fix: CI issues 4

--- smartContracts/hardhat.config.js
+++ smartContracts/hardhat.config.js
@@ -6,9 +6,12 @@ require('dotenv').config();  // Add this line to load .env variables
 module.exports = {
   solidity: "0.8.27",
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:301"
+    },
     sepolia: {
       url: process.env.TESTNET_URL,
-      accounts: [process.env.TESTNET_PRIVATE_KEY], 
+      accounts: [process.env.TESTNET_PRIVATE_KEY],
     },
   },
 };
