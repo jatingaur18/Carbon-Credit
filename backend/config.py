739eb2190b05cdd43d4a4eeaddf46e08b513094c
R0gue-one <harshitjaiswal2606@gmail.com>
2025-01-19 23:35:42 +0530

    backend undo

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,7 @@
 from datetime import timedelta
-from sqlalchemy import *
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlitecloud://cf5em6kdnz.g1.sqlite.cloud:8860/chinook.sqlite?apikey=mqomFWcq7RkkPJOzaAWAyhLUQl3P1LGaMNYmXpieb90'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///carbon_credits.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'your-secret-key'
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
