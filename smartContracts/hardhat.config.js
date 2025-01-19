91db74877e6c39c1d66fe3e84863d7ed5e32afb8
Devansh Srivastava <devansh.srv@gmail.com>
2025-01-19 19:48:28 +0530

    fix: CI issue 5

--- smartContracts/hardhat.config.js
+++ smartContracts/hardhat.config.js
@@ -7,11 +7,11 @@ module.exports = {
   solidity: "0.8.27",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:301"
+      url: "http://127.0.0.1:8545"
     },
     sepolia: {
-      url: process.env.TESTNET_URL,
-      accounts: [process.env.TESTNET_PRIVATE_KEY],
+      url: process.env.TESTNET_URL || "http://127.0.0.1:8545",
+      accounts: process.env.TESTNET_PRIVATE_KEY ? [process.env.TESTNET_PRIVATE_KEY] : [],
     },
   },
 };
