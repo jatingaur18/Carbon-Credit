3b22c1402f9376ef90987fb7a1a49fb8056bb45d
Devansh Srivastava <devansh.srv@gmail.com>
2025-01-30 18:35:47 +0530

    fix: neonDB pool_recycle

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -6,7 +6,7 @@ from flask_jwt_extended import JWTManager,create_access_token, jwt_required,get_
 from flask_cors import CORS
 from config import Config
 
-db = SQLAlchemy()
+db = SQLAlchemy(engine_options=Config.SQLALCHEMY_ENGINE_OPTIONS)
 bcrypt = Bcrypt()
 jwt = JWTManager()
 
--- client/src/components/BuyerSignup.js
+++ client/src/components/BuyerSignup.js
@@ -8,6 +8,8 @@ const BuyerSignup = ({ onLogin }) => {
   const [formData, setFormData] = useState({ username: '', email: '', password: '' });
   const [captchaToken, setCaptchaToken] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+  const [status, setStatus] = useState(null)
+  const [color, setColor] = useState(null)
   const navigate = useNavigate();
   const SITE_KEY = process.env.REACT_APP_SITE_KEY || '1x00000000000000000000AA';
   const handleChange = (e) => {
@@ -22,7 +24,13 @@ const BuyerSignup = ({ onLogin }) => {
     e.preventDefault();
     try {
       console.log("FormData:", formData);
-      await signup({ ...formData, role: 'buyer', 'cf-turnstile-response': captchaToken });
+      const signupResponse = await signup({ ...formData, role: 'buyer', 'cf-turnstile-response': captchaToken });
+      setStatus(signupResponse.data.message)
+      setColor('bg-indigo-700')
+      setInterval(() => {
+        setStatus(null)
+        setColor(null)
+      }, 3000)
       const loginResponse = await login({ ...formData, role: 'buyer', 'cf-turnstile-response': captchaToken });
 
       localStorage.setItem("token", loginResponse.data.access_token);
@@ -31,13 +39,28 @@ const BuyerSignup = ({ onLogin }) => {
       navigate('/buyer-dashboard');
 
     } catch (error) {
-      console.error('Signup failed:', error);
+      if (error.status === 400) { alert(error?.response?.data?.message) } else {
+        console.error('Signup failed:', error);
+        setStatus('Signup Failed. Please Try Again')
+        setColor('bg-red-500')
+        setInterval(() => {
+          setStatus(null)
+          setColor(null)
+        }, 3000)
+      }
 
     }
   };
 
   return (
     <div className="flex justify-center items-center py-12 px-4 w-full min-h-screen bg-gradient-to-br from-indigo-500 to-blue-400">
+
+      {status && (
+        <div className={`flex fixed top-5 left-1/2 items-center py-2 px-4 text-white ${color} rounded-lg shadow-lg transition-transform duration-300 transform -translate-x-1/2 animate-slideIn}`} >
+          <span>{status}</span>
+        </div>
+      )
+      }
       <div className="w-full max-w-md rounded-xl shadow-xl bg-white/90 backdrop-blur-sm">
         <div className="p-8">
           <div className="mb-1 text-sm font-semibold tracking-wide text-blue-700 uppercase">Buyer Registration</div>
