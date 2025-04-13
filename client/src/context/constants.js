a5ce787d9749dfed9c9686bab5d41522636a3a0e
Devansh Srivastava <devansh.srv@gmail.com>
2025-04-13 22:05:10 +0530

    fix: contract hash

--- client/src/context/constants.js
+++ client/src/context/constants.js
@@ -1,4 +1,4 @@
 import CarbonCredit from './CarbonCredit.json';
-// export const CC_ADDRESS = '0x15Ef15a50a2F72126EBcDC6989044Ce6ae322802';
-export const CC_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const CC_ADDRESS = '0x15Ef15a50a2F72126EBcDC6989044Ce6ae322802'; //this should be in the actual repo
+// export const CC_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3'; for testing
 export const CC_ABI = CarbonCredit.abi;
