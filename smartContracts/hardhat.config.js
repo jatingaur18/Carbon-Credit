f04d20375420e2dd0a9f7e255f70597e9b140cb5
Devansh Srivastava <devansh.srv@gmail.com>
2025-04-15 11:25:56 +0530

    fix: contract hash again

--- smartContracts/hardhat.config.js
+++ smartContracts/hardhat.config.js
@@ -9,6 +9,10 @@ module.exports = {
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545"
+    },
+    sepolia: {
+      url: TESTNET_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
     }
   },
 };
